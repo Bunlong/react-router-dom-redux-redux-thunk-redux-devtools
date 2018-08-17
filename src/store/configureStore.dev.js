4e6ff9e767a980e699089d4ee2aa15d14765be01
bunlong <bunlong.van@gmail.com>
2018-08-17 17:11:23 +0700

    add note and config

---
+++ src/store/configureStore.dev.js
@@ -0,0 +1,36 @@
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import api from '../middleware/api'
+import rootReducer from '../reducers'
+import DevTools from '../containers/DevTools'
+
+const configureStore = preloadedState => {
+  const store = createStore(
+    rootReducer,
+    preloadedState,
+    compose(
+      applyMiddleware(thunk, api, createLogger()),
+      DevTools.instrument()
+    )
+  )
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      store.replaceReducer(rootReducer)
+    })
+  }
+
+  return store
+}
+
+export default configureStore
+
+const configureStore = preloadedState => createStore(
+  rootReducer,
+  preloadedState,
+  applyMiddleware(customMiddleWare, incrementMiddleware),
+)
+
+export default configureStore
